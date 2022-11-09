63f3e6a2036ad7c915303bbb47a2b189843ec3f9
bhabna01 <abierhoque01@gmail.com>
2022-11-09 20:02:29 +0600

    5th commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config();
 const app = express();
@@ -65,12 +65,12 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services);
         })
-        // app.get('/services/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: ObjectId(id) };
-        //     const service = await serviceCollection.findOne(query)
-        //     res.send(service);
-        // })
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query)
+            res.send(service);
+        })
         app.post('/services', async (req, res) => {
             const service = req.body;
             const result = await serviceCollection.insertOne(service);
