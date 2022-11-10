898daa7449ecc84685ed46b8f3579c05b776ff28
bhabna01 <abierhoque01@gmail.com>
2022-11-10 16:57:37 +0600

    9th commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
+// const jwt = require('jsonwebtoken')
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -47,8 +47,9 @@ async function run() {
         const reviewCollection = client.db('esthetica').collection('reviews')
         // app.post('/jwt', (req, res) => {
         //     const user = req.body;
-        //     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1hr' })
-        //     res.send({ token })
+        //     console.log(user);
+        //     // const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1hr' })
+        //     // res.send({ token })
         // })
 
 
